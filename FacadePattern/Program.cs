d764afbea5cc0e118ba3a109840c9c30245dc537
lunawen <xinw@resonatesolutions.com.au>
2022-03-12 16:32:31 +1100

    Add project files.

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using BallOfMud.Services;
+
+/*
+ * Without using Facade
+
+BigClass bigClass = new BigClass();
+            
+bigClass.SetValueI(3);
+            
+bigClass.IncrementI();
+bigClass.IncrementI();
+bigClass.IncrementI();
+            
+bigClass.DecrememntI();
+
+Console.WriteLine($"Final Number : {bigClass.GetValueB()}");
+ */
+
+
+// Using Facade
+
+BigClassFacade bigClass = new BigClassFacade();
+
+bigClass.IncreaseBy(50);
+bigClass.DecreaseBy(20);
+
+Console.WriteLine($"Final Number : {bigClass.GetCurrentValue()}");
