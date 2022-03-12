fc5e03ee617bf3b18b26ee12d0f0d9c9a3915458
lunawen <xinw@resonatesolutions.com.au>
2022-03-12 16:49:30 +1100

    generic facade - start

---
+++ GenericFacade/Program.cs
@@ -0,0 +1,10 @@
+﻿ServiceA serviceA = new ServiceA();
+int sAResult = serviceA.Method2();
+
+ServiceB serviceB = new ServiceB();
+string sBResult = serviceB.Method2();
+
+ServiceC serviceC = new ServiceC();
+double sCResult = serviceC.Method1();
+
+Console.WriteLine(sAResult + " - " + sCResult + " - " + sBResult);
